ea785af7fccf128d18c9409fcd091ae47131de8f
Artiom U <arti.urb@gmail.com>
2020-02-02 14:44:25 +0100

    commit v4

--- src/main/java/Prohoroscope/App.java
+++ src/main/java/Prohoroscope/App.java
@@ -3,21 +3,26 @@ package Prohoroscope;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("podaj dane!");
-        String znak = scan.next();
+        System.out.println("podaj dane: IMIE!");
+        String userName = scan.next();
+        System.out.println("dzien");
+        Integer day = scan.nextInt();
+        scan.nextLine();
 
-        String requestResult = Connector.getPage(znak);
+        System.out.println("miesiac");
+        Integer month = scan.nextInt();
+        String sign = ZodiacSignRecogniser.getSign(day, month);
+
+
+        String requestResult = Connector.getPage(sign);
         String horoscopeMess = " ";
         if (!requestResult.equals(Connector.NOT_FOUND)) {
-         horoscopeMess = HOROSCOPEXTRACTOR.getText(requestResult);
+            horoscopeMess = HOROSCOPEXTRACTOR.getText(requestResult);
         }
         System.out.println(horoscopeMess);
-
-
     }
-    }
+}
